9cc6aae72e092626499fdebcfb27499a2dcd2655
Lucia320 <lucia.320@gmail.com>
2017-12-28 08:31:09 -0500

    Servicio eureka

---
+++ demo-eureka-server/src/test/java/co/com/ingeneo/demo/eureka/DemoEurekaServerApplicationTests.java
@@ -0,0 +1,19 @@
+package co.com.ingeneo.demo.eureka;
+
+/*
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
+*/
